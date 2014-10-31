393738a7cd6232f86af9994cc7b9918952ea54fb
Austin Bourgerie <bourgerie.a@gmail.com>
2014-10-30 21:07:09 -0400

    Added DeathDropBlocker to allow dynamic choice of death drops

--- src/core/CoreListener.java
+++ src/core/CoreListener.java
@@ -2,17 +2,10 @@ package core;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 
 public class CoreListener implements Listener 
 {
-	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event){
-		event.getDrops().clear();
-		event.setDroppedExp(0);
-	}
-	
 	@EventHandler
 	public void onPlayerItemBreak(PlayerItemBreakEvent event)
 	{
---
+++ src/core/Utilities/DeathDropBlocker.java
@@ -0,0 +1,36 @@
+package core.Utilities;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import core.Custody.CustodySwitchEvent;
+
+public class DeathDropBlocker implements Listener
+{
+	private static ArrayList<UUID> canDrop = new ArrayList<UUID>();
+	
+	public static void setCanDrop(UUID playerUUID)
+	{
+		canDrop.add(playerUUID);
+	}
+
+	@EventHandler
+	public void onCustodySwitch(CustodySwitchEvent event)
+	{
+		canDrop.remove(event.getPlayer().getUniqueId());
+	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event)
+	{
+		if (!canDrop.contains(event.getEntity().getUniqueId()))
+		{
+			event.setDroppedExp(0);
+			event.getDrops().clear();
+		}
+	}
+}
